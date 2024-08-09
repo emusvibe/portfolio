e45408fbb6b223be4dd7aadb1d874020488ad5ce
Edmore Musvibe <edmmusvi@WKMZT811A137.global.publicisgroupe.net>
2024-08-09 22:06:11 +0200

    Added featured section

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import "../styles/mordern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/about.css";
 import "../styles/utils.css";

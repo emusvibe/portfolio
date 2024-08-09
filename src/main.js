a7824691dea7defe85cdd255ad14dfd0290840d7
Edmore Musvibe <edmmusvi@WKMZT811A137.global.publicisgroupe.net>
2024-08-09 19:59:02 +0200

    Added hero image

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "../styles/mordern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
+import "../styles/components/hero.css";
 import "../styles/utils.css";
